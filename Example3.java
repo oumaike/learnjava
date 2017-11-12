c05a26ac180aea83dfa1034ad791032781e034ff
DESKTOP-4UHVUH2\oumai <oumaike@gmail.com>
2017-11-11 18:50:13 -0800

    new files

---
+++ Example3.java
@@ -0,0 +1,28 @@
+/*
+	This program illustrates the differences
+	between int and double.
+*/
+// adding a random comment
+// adding another random comment
+class Example3 {
+	public static void main(String args[]) {
+		int var; 
+		double x;
+		
+		var = 10;
+		
+		x = 10.0;
+		
+		System.out.println("Original value of var: " + var);
+		System.out.println("Original value of x: " + x);
+		System.out.println(); //print a blank line
+		
+		//now divide both by 4
+		var = var / 4;
+		x = x / 4; 
+		
+		System.out.println("var after division: " + var);
+		System.out.println("x after diviion: " + x);
+
+	}
+}
